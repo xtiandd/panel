a14461c05be211716119603a5b86858172b309b7
ilworkcn <1061372739@qq.com>
2021-11-01 22:02:08 +0800

    translate stage 5
    translate some little small things?

--- resources/scripts/components/server/StatGraphs.tsx
+++ resources/scripts/components/server/StatGraphs.tsx
@@ -131,28 +131,28 @@ export default () => {
     return (
         <div css={tw`flex flex-wrap mt-4`}>
             <div css={tw`w-full sm:w-1/2`}>
-                <TitledGreyBox title={'Memory usage'} icon={faMemory} css={tw`mr-0 sm:mr-4`}>
+                <TitledGreyBox title={'运行内存使用率'} icon={faMemory} css={tw`mr-0 sm:mr-4`}>
                     {status !== 'offline' ?
                         <canvas
                             id={'memory_chart'}
                             ref={memoryRef}
-                            aria-label={'Server Memory Usage Graph'}
+                            aria-label={'服务器运行内存使用率'}
                             role={'img'}
                         />
                         :
                         <p css={tw`text-xs text-neutral-400 text-center p-3`}>
-                            Server is offline.
+                            服务器未运行.
                         </p>
                     }
                 </TitledGreyBox>
             </div>
             <div css={tw`w-full sm:w-1/2 mt-4 sm:mt-0`}>
-                <TitledGreyBox title={'CPU usage'} icon={faMicrochip} css={tw`ml-0 sm:ml-4`}>
+                <TitledGreyBox title={'CPU 使用率'} icon={faMicrochip} css={tw`ml-0 sm:ml-4`}>
                     {status !== 'offline' ?
-                        <canvas id={'cpu_chart'} ref={cpuRef} aria-label={'Server CPU Usage Graph'} role={'img'}/>
+                        <canvas id={'cpu_chart'} ref={cpuRef} aria-label={'服务器 CPU 使用率'} role={'img'}/>
                         :
                         <p css={tw`text-xs text-neutral-400 text-center p-3`}>
-                            Server is offline.
+                            服务器未运行.
                         </p>
                     }
                 </TitledGreyBox>
