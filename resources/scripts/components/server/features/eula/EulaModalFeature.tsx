6fe841ca8c8f1b2bea6e69428859ef8f88f64191
ilworkcn <1061372739@qq.com>
2021-11-02 16:23:03 +0800

    translate backup components
    rt

--- resources/scripts/components/server/features/eula/EulaModalFeature.tsx
+++ resources/scripts/components/server/features/eula/EulaModalFeature.tsx
@@ -21,7 +21,7 @@ const EulaModalFeature = () => {
         if (!connected || !instance || status === 'running') return;
 
         const listener = (line: string) => {
-            if (line.toLowerCase().indexOf('you need to agree to the eula in order to run the server') >= 0) {
+            if (line.toLowerCase().indexOf('你需要同意 Mojang.AB 的 EULA 协议后才能继续！') >= 0) {
                 setVisible(true);
             }
         };
@@ -60,24 +60,24 @@ const EulaModalFeature = () => {
     return (
         <Modal visible={visible} onDismissed={() => setVisible(false)} closeOnBackground={false} showSpinnerOverlay={loading}>
             <FlashMessageRender key={'feature:eula'} css={tw`mb-4`}/>
-            <h2 css={tw`text-2xl mb-4 text-neutral-100`}>Accept Minecraft&reg; EULA</h2>
+            <h2 css={tw`text-2xl mb-4 text-neutral-100`}>同意 Minecraft&reg; EULA 协议</h2>
             <p css={tw`text-neutral-200`}>
-                    By pressing {'"I Accept"'} below you are indicating your agreement to the&nbsp;
+                    请点击 {'"我接受"'} 来同意&nbsp;
                 <a
                     target={'_blank'}
                     css={tw`text-primary-300 underline transition-colors duration-150 hover:text-primary-400`}
                     rel={'noreferrer noopener'}
                     href="https://account.mojang.com/documents/minecraft_eula"
                 >
-                        Minecraft&reg; EULA
+                        Minecraft&reg; EULA 协议
                 </a>.
             </p>
             <div css={tw`mt-8 sm:flex items-center justify-end`}>
                 <Button isSecondary onClick={() => setVisible(false)} css={tw`w-full sm:w-auto border-transparent`}>
-                        Cancel
+                        不接受
                 </Button>
                 <Button onClick={onAcceptEULA} css={tw`mt-4 sm:mt-0 sm:ml-4 w-full sm:w-auto`}>
-                        I Accept
+                        我接受
                 </Button>
             </div>
         </Modal>
